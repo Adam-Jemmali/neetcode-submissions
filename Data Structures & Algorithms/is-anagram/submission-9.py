e3a06b6597f1eb2003b3f3bdd1a17b390c2d6997
Adam Jemmali <trapdimej@gmail.com>
2026-09-12 12:44:54 -0400

    Add: is-anagram - submission-9

---
+++ Data Structures & Algorithms/is-anagram/submission-9.py
@@ -0,0 +1,10 @@
+from collections import Counter
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+
+        #baes case not anagram if diff len strings:
+        if len(s) != len(t):
+            return False
+        return Counter(s)==Counter(t)
+        #sorted o log n + o( m log m )
+        
